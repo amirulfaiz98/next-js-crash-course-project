c5e7f87fa618191ec8ca231432c28a48539e55f7
Amirul Faiz <faiz@baituljannah.com>
2024-02-08 19:59:20 +0800

    Loading Data & Paths For Dynamic Pages

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -1,20 +1,27 @@
 export async function getAllEvents() {
-    const response =  await fetch('https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json'); //dummy url, later create real firebase
-    const data = await response.json();
+  const response = await fetch(
+    "https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json"
+  ); //dummy url, later create real firebase
+  const data = await response.json();
 
-    const events = [];
+  const events = [];
 
-    for (const key in data) {
-        events.push({
-            id: key,
-            ...data[key],
-        });
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
 
-        return events;
-    }
+    return events;
+  }
 }
 
 export async function getFeaturedEvents() {
-    const allEvents = await getAllEvents();
-    return allEvents.filter((event) => event.isFeatured);
-  }
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
+
+export async function getEventById(id) {
+  const allEvents = await getAllEvents();
+  return allEvents.find((event) => event.id === id);
+}
